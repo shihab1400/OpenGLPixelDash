ac5e5c000383bf66ccde0922c3bcaa89dd5acab3
Farhat <shihabcoc39@gmail.com>
2025-12-21 22:10:20 +0600

    Collision Detection V2

--- collisionDetection.cpp
+++ collisionDetection.cpp
@@ -10,9 +10,9 @@ struct Rect
 
 bool collision(const Rect& a, const Rect& b) {
 
-    bool collisionX = a.x + a.width  >= b.x && b.x + b.width  >= a.x;
+    bool collisionX = a.x + a.width  > b.x && b.x + b.width  > a.x;
 
-    bool collisionY = a.y + a.height >= b.y && b.y + b.height >= a.y;
+    bool collisionY = a.y + a.height > b.y && b.y + b.height > a.y;
 
     return collisionX && collisionY;
 }
